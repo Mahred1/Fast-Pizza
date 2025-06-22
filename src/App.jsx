57f1e8734f65014b4b41620a0505320e8d3e4859
mahred1 <mahred.kemal@gmail.com>
2025-06-22 21:01:33 +0300

    added order searching with order Id

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import Applayout from "./ui/Applayout";
 import Error from "./ui/Error";
 
@@ -34,6 +34,8 @@ function App() {
         {
           path: "/order/:orderId",
           element: <Order />,
+          loader: orderLoader,
+          errorElement: <Error />,
         },
       ],
     },
